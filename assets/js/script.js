f955d5b78a4123b48d7c6287ec5238a4af06eb0b
rover-7-7 <rover-7-7@users.noreply.github.com>
2025-01-31 18:44:55 +0000

    deploy: 40e0d4256d2b200a265decc4d128cf9c723bd519

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,7 +64,7 @@ $(document).ready(function () {
 
 document.addEventListener("visibilitychange", function () {
   if (document.visibilityState === "visible") {
-    document.title = "Portfolio | Jigar Sable";
+    document.title = "Portfolio | Prince Kumar";
     $("#favicon").attr("href", "assets/images/favicon.png");
   } else {
     document.title = "Come Back To Portfolio";
